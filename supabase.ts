72c03d097feaa919af9d454e9511a335097df868
IT1M <162412258+IT1M@users.noreply.github.com>
2025-06-06 18:05:43 +0300

    Create supabase.ts

---
+++ supabase.ts
@@ -0,0 +1,4 @@
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://qovspvegmjvslzizibxr.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFvdnNwdmVnbWp2c2x6aXppYnhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyMTU0NjcsImV4cCI6MjA2NDc5MTQ2N30.RNvl_GWX8fWNnGi2Nkq4HPVTYFbhKamfygdvtG55bOY'
+const supabase = createClient(supabaseUrl, supabaseKey)
